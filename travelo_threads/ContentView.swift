a98938d868c945891131b087023289661396bf8b
danuja01 <it21023378@my.sliit.lk>
2024-06-06 17:49:19 +0530

    Initial Commit

---
+++ travelo_threads/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  travelo_threads
+//
+//  Created by Danuja Jayasuriya on 2024-06-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ travelo_threads/travelo_threadsApp.swift
@@ -0,0 +1,17 @@
+//
+//  travelo_threadsApp.swift
+//  travelo_threads
+//
+//  Created by Danuja Jayasuriya on 2024-06-06.
+//
+
+import SwiftUI
+
+@main
+struct travelo_threadsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
