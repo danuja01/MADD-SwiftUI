1c4a2e20b72f959512f7d289cb97729ff7da11a7
danuja01 <it21023378@my.sliit.lk>
2024-06-07 11:13:43 +0530

    Impliment Thread card

---
+++ travelo_threads/Components/ThreadCard/CardButtons.swift
@@ -0,0 +1,18 @@
+//
+//  CardButtons.swift
+//  travelo_threads
+//
+//  Created by Danuja Jayasuriya on 2024-06-07.
+//
+
+import SwiftUI
+
+struct CardButtons: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardButtons()
+}
---
+++ travelo_threads/Models/Thread.swift
@@ -0,0 +1,8 @@
+//
+//  Thread.swift
+//  travelo_threads
+//
+//  Created by Danuja Jayasuriya on 2024-06-07.
+//
+
+import Foundation
