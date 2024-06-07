91c865a88829b42fb2acebab36297cc66c06e05b
danuja01 <it21023378@my.sliit.lk>
2024-06-07 19:16:15 +0530

    Impliment Thread model

--- travelo_threads/Models/Thread.swift
+++ travelo_threads/Models/Thread.swift
@@ -5,38 +5,70 @@
 //  Created by Danuja Jayasuriya on 2024-06-07.
 //
 import SwiftUI
+import CoreLocation
+import Foundation
 
 struct Thread: Identifiable {
-    var id = UUID()
+    var id: String
     var title: String
     var caption: String
-    var color:String
-    var image: Image
+    var imageUrl: String?
+    var location: CLLocationCoordinate2D
+    var userImage: String
+    var userName: String
+    var favoriteCount: Int
 }
 
 var sampleThreads = [
     Thread(
-        title: "Mountain Adventure",
-        caption: "Exploring the heights of the Rockies.",
-        color: "Blue",
-        image: Image("Sigiriya")
+        id: "1",
+        title: "Mountain Hike",
+        caption: "Enjoying the breathtaking views of the Alps.",
+        imageUrl: "https://ichef.bbci.co.uk/images/ic/1376xn/p0b7n6dm.jpg.webp",
+        location: CLLocationCoordinate2D(latitude: 46.0207, longitude: 7.7491),
+        userImage: "https://shorturl.at/oC3I3",
+        userName: "Alice Hudson",
+        favoriteCount: 15
     ),
     Thread(
-        title: "City Life",
-        caption: "Discovering the hustle and bustle of urban centers.",
-        color: "Gray",
-        image: Image("cityImage")
+        id: "2",
+        title: "Way to Sigiriya",
+        caption: "Lats day I wen to Sigiriya with my friends and it was and unbelievable construction on a Rock!!",
+        imageUrl: nil,
+        location: CLLocationCoordinate2D(latitude: 7.956944, longitude: 80.759720 ),
+        userImage: "https://shorturl.at/oC3I3",
+        userName: "Bob Marley",
+        favoriteCount: 8
     ),
     Thread(
-        title: "Desert Exploration",
-        caption: "The beauty and isolation of the desert.",
-        color: "Orange",
-        image: Image("desertImage")
+        id: "3",
+        title: "Desert Safari",
+        caption: "An adventure in the Sahara Desert.",
+        imageUrl: "https://ichef.bbci.co.uk/images/ic/1376xn/p0b7n6dm.jpg.webp",
+        location: CLLocationCoordinate2D(latitude: 23.4162, longitude: 25.6628),
+        userImage: "https://shorturl.at/oC3I3",
+        userName: "Carol Spat",
+        favoriteCount: 20
     ),
     Thread(
-        title: "Ocean Views",
-        caption: "Serenity and vastness of the open water.",
-        color: "Teal",
-        image: Image("oceanImage")
+        id: "4",
+        title: "Ocean Retreat",
+        caption: "Relaxing by the serene Maldives beaches.",
+        imageUrl: nil,
+        location: CLLocationCoordinate2D(latitude: 1.9772, longitude: 73.5361),
+        userImage: "https://shorturl.at/oC3I3",
+        userName: "Dave John",
+        favoriteCount: 25
+    ),
+    Thread(
+        id: "5",
+        title: "Forest Camping",
+        caption: "Camping in the mystic woods of the Amazon.",
+        imageUrl: "https://ichef.bbci.co.uk/images/ic/1376xn/p0b7n6dm.jpg.webp",
+        location: CLLocationCoordinate2D(latitude: -3.4653, longitude: -62.2159),
+        userImage: "https://shorturl.at/oC3I3",
+        userName: "Eva Clark",
+        favoriteCount: 10
     )
 ]
+
